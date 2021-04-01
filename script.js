abee3e0af797dae7a5a434f5d4663d1998b9dc92
Jay persad <jasonprogrammer91@gmail.com>
2021-04-01 16:07:49 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+/* var list = document.querySelector('#list')
+var fruits = ['Apple', 'Orange', 'Banana', 'Melon',"kiwi"]
+
+var fragment = new DocumentFragment()
+
+fruits.forEach(function (fruit) {
+  var li = document.createElement('li')
+  li.innerHTML = fruit
+  fragment.appendChild(li)
+})
+
+list.appendChild(fragment) */
+
+
+
